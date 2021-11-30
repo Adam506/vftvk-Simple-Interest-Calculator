b814903e62bcfede5b33b2b88c37cb9c6c844060
astaa <52228381+Adam506@users.noreply.github.com>
2021-11-30 17:57:51 +0800

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+    i = document.getElementById("result").value;
+
+    let totalInterest = (r*y*p)/100;
+    let year = 2020;
+    y = parseInt(y);
+    const total = (year + y);
+    if (p || r || y <= 0)
+    {
+        alert("please input positive number");
+        document.getElementById('principal').value = ''
+        document.getElementById('years').value = ''
+    }
+    document.getElementById('result').innerHTML=
+    "If you deposit " + p + "<br>" +
+    'at an interest rate of ' + r + '%' + "<br>" +
+    'You will receive an amount of ' + totalInterest + "<br>" +
+    'in the year ' + total;
     
+   
 }
-        
+   
